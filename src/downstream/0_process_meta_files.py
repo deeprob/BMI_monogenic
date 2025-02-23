cd6a04172635722b75e381abd2ff4b5de99a5d30
Deepro Banerjee <deeprobanerjee@Deepros-MacBook-Air.local>
2025-02-22 23:40:40 -0500

    known-genes

--- src/downstream/0_process_meta_files.py
+++ src/downstream/0_process_meta_files.py
@@ -13,20 +13,22 @@ if __name__=="__main__":
         hgnc_file, sep="\t", 
         usecols=["hgnc_id", "symbol", "name", "alias_symbol", "alias_name", "prev_symbol", "prev_name"]
     )
-    analyses = ["all_ancestry", "lovo", "meds", "shadow_effect"]
-    meta_classes = [meta.Meta, meta.Lovo, meta.Meds, meta.Meds]
-    phenotypes =[("bmi_rint", "bmi"), ("bmi_rint", ), ("bmi_rint", ), ("bmi_rint", )]
+    analyses = ["all_ancestry", "lovo", "meds", "shadow_effect", "male", "female"]
+    meta_classes = [meta.Meta, meta.Lovo, meta.Meds, meta.Meds, meta.Meta, meta.Meta]
+    phenotypes =[("bmi_rint", "bmi"), ("bmi_rint", ), ("bmi_rint", ), ("bmi_rint", ), ("bmi_rint", ), ("bmi_rint", )]
+    reruns = [False, False, False, False, True, True]
 
-    for analysis, meta_class, phenos in zip(analyses, meta_classes, phenotypes):
-        meta_class = meta_class()
-        for biobank in ["aou", "ukb"]:
-            for ancestry in ["afr", "amr", "eas", "eur", "sas", "mid"]:
-                for pheno in phenos:
-                    filename = f"{pheno}_{ancestry}_{biobank}_meta_w_samples.tsv.gz"
-                    filepath = os.path.join(PROJECT_DIR, "data/meta/raw", analysis, filename)
-                    meta_df = pd.read_csv(filepath, sep="\t")
-                    processed_meta_df = meta_class.harmonize_gene_symbols_and_filter(meta_df, hgnc_df)
-                    save_filename = f"{pheno}_{ancestry}_{biobank}.tsv.gz"
-                    save_filepath =  os.path.join(PROJECT_DIR, "data/meta/processed", analysis, save_filename)
-                    os.makedirs(os.path.dirname(save_filepath), exist_ok=True)
-                    processed_meta_df.to_csv(save_filepath, sep="\t", index=False)
+    for analysis, meta_class, phenos, rerun in zip(analyses, meta_classes, phenotypes, reruns):
+        if rerun:
+            meta_class = meta_class()
+            for biobank in ["aou", "ukb"]:
+                for ancestry in ["afr", "amr", "eas", "eur", "sas", "mid"]:
+                    for pheno in phenos:
+                        filename = f"{pheno}_{ancestry}_{biobank}_meta_w_samples.tsv.gz"
+                        filepath = os.path.join(PROJECT_DIR, "data/meta/raw", analysis, filename)
+                        meta_df = pd.read_csv(filepath, sep="\t")
+                        processed_meta_df = meta_class.harmonize_gene_symbols_and_filter(meta_df, hgnc_df)
+                        save_filename = f"{pheno}_{ancestry}_{biobank}.tsv.gz"
+                        save_filepath =  os.path.join(PROJECT_DIR, "data/meta/processed", analysis, save_filename)
+                        os.makedirs(os.path.dirname(save_filepath), exist_ok=True)
+                        processed_meta_df.to_csv(save_filepath, sep="\t", index=False)
